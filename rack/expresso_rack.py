e1b15746a5e028c014d16f67a42ed693064c4ed6
Cisco Zabala <xiscolab@gmail.com>
2012-10-02 18:09:57 -0700

    working on reconfigurable rack.

--- rack/expresso_rack.py
+++ rack/expresso_rack.py
@@ -1,12 +1,12 @@
 from __future__ import division
 from subprocess import Popen,PIPE
 from py2scad import *
-import glob, os
-
-#x_r = N*2.5*INCH2MM + 2.*x_r_overhang
-#y_r = y_e + 2.*wall_thickness_rack + y_r_overhang 
+import glob, os, math
 
 class Expresso_Rack(object):
+    DEG2RAD = math.pi/180
+    RAD2DEG = 180/math.pi
+
     def __init__(self, params):
         self.params = params
 
@@ -16,16 +16,199 @@ class Expresso_Rack(object):
         self.__make_holders()
         self.__make_bracket()
     
+    #########################################
+    # Rack Components
+    #########################################
     def __make_walls(self):
-        pass
+        """
+        Creates the left and right walls of the racki (plate w/ tabs).
+        """
+        x,y,z = self.params['inner_dimensions']
+        thickness = self.params['wall_thickness']
+        wall_x_overhang = self.params['x_r_overhang']
+        d_tab = self.params['wall_tab_dist']
+        tab_width = self.params['wall_tab_width']
+        dia = self.params['wall_hole_dia_thru']
+        wall_hole_y_offset = self.params['wall_hole_y_offset']
+        tab_depth = thickness
+        x_w = x + 2*wall_x_overhang
+        y_w = z
+        z_w = thickness
+
+        # Tab and hole (for l-brackets) data for stabilizing the walls
+        xz_neg = []
+        hole_list = []
+        for i in range(0,2*self.params['num_devices'],2):
+            # Create tab data for xz- face of walls
+            tab_x = wall_x_overhang + (i+1)*d_tab
+            tab_data = (tab_x/x_w, tab_width, tab_depth, '+')
+            xz_neg.append(tab_data)
+
+            # Create hole data for walls
+            if i < 2*self.params['num_devices']-2:
+                hole_x = (i+1)*d_tab - .5*x + d_tab
+                hole_y = -.5*y_w + wall_hole_y_offset
+                hole_data = (hole_x,hole_y,dia)
+                hole_list.append(hole_data)
+            # Special case for a 1-device rack
+            if self.params['num_devices'] == 1:
+                hole_x = (i+1)*d_tab - .5*x
+                hole_y = -.5*y_w + wall_hole_y_offset
+                hole_data = (hole_x,hole_y,dia)
+                hole_list.append(hole_data)
+
+        # End holes of the walls, for stability rods
+        hole_x_offset = .5*self.params['stability_rod_dia']
+        for i in (-1,1):
+            for j in (-1,1):
+                hole_x = i*j*.5*x_w - j*hole_x_offset
+                hole_y = 0
+                hole_data = (hole_x,hole_y,dia)
+                hole_list.append(hole_data)
+
+        holder_hole_offset = self.params['holder_hole_offset']
+
+        # End holes for the holders
+        for i in (-1,1):
+            for j in (-1,1):
+                hole_x = i*j*.5*x - j*d_tab
+                for dy in holder_hole_offset:
+                    hole_y = dy
+                    hole_data = (hole_x,hole_y,dia)
+                    hole_list.append(hole_data)
+        # Special cases for a 5-device (or greater) rack
+        # need to add an extra support hole in the middle
+        if self.params['num_devices'] >= 5:
+            for dy in holder_hole_offset:
+                hole_data = (0,dy,dia)
+                hole_list.append(hole_data)
+
+        # Pack data into parameters structure
+        params = {
+                'size'      : (x_w, y_w, z_w),
+                'radius'    : self.params['corner_radius'], 
+                'xz+'       : [],
+                'xz-'       : xz_neg,
+                'yz+'       : [],
+                'yz-'       : [],
+                'hole_list' : hole_list,
+        }  
+
+        plate_maker = Plate_W_Tabs(params)
+        self.left_wall = plate_maker.make()
+        self.right_wall = plate_maker.make()
+
     def __make_floor(self):
-        pass
+        """
+        Creates the floor of the enclosure (plate w/ slots).
+        """
+        x,y,z = self.params['inner_dimensions']
+        thickness = self.params['wall_thickness']
+        floor_x_overhang = self.params['x_r_overhang']
+        floor_y_overhang = self.params['y_r_overhang']
+        d_slot = self.params['wall_tab_dist']
+        slot_width = self.params['wall_tab_width']
+        tol = self.params['floor_slot_tol']
+        dia = self.params['floor_hole_dia_thru']
+        floor_hole_y_offset = self.params['floor_hole_y_offset']
+
+        x_f = x + 2*floor_x_overhang
+        y_f = y + 2*thickness + 2*floor_y_overhang
+        z_f = thickness
+
+        # Create slot data for rack floor
+        slot_list = []
+        hole_list = []
+        size = (slot_width-tol[0], thickness-tol[1])
+        for i in range(0,2*self.params['num_devices'],2):
+            slot_x = (i+1)*d_slot-.5*x
+            for j in (-1,1):
+                pos = (slot_x,.5*j*(y+thickness))
+                slot_list.append((pos,size))
+                # Create hole data for walls
+                if i < 2*self.params['num_devices']-2:
+                    hole_x = slot_x + d_slot
+                    hole_y = -.5*y_f + floor_hole_y_offset
+                    hole_data = (hole_x,j*hole_y,dia)
+                    hole_list.append(hole_data)
+                # Special case for a 1-device rack
+                if self.params['num_devices'] == 1:
+                    hole_x = slot_x
+                    hole_y = -.5*y_f + floor_hole_y_offset
+                    hole_data = (hole_x,j*hole_y,dia)
+                    hole_list.append(hole_data)
+
+        # Pack data into parameters structure
+        params = {
+            'size'      : (x_f,y_f,z_f),    
+            'radius'    : self.params['corner_radius'], 
+            'slots'     : slot_list,  
+            'hole_list' : hole_list
+        }
+        plate_maker = Plate_W_Slots(params)
+        self.floor = plate_maker.make()
+
     def __make_holders(self):
-        pass
+        """
+        Creates the left and right 1/4" holders into which the Expresso enclosures
+        are placed (custom plate w/ slots)
+        """
+        x,y,z = self.params['inner_dimensions']
+        x_h,foo,bar = self.params['inner_dimensions']
+        thickness = self.params['wall_thickness']
+        thickness_enc = self.params['wall_thickness_enc']
+        holder_slot_size = self.params['holder_slot_size']
+        d_slot = self.params['wall_tab_dist']
+        dia = self.params['wall_hole_dia_tap']
+
+        y_h = self.params['holder_height']
+        z_h = thickness
+
+        r = .5*holder_slot_size[1]
+        r_t = .5*thickness_enc
+        theta = self.params['tilt_angle']*self.DEG2RAD
+        holder_slot_y_offset = .5*y_h + r_t*math.cos(theta) - r*math.cos(theta)
+
+        slot_list = []
+        hole_list = []
+        for i in range(0,2*self.params['num_devices'],2):
+            slot_x = (i+1)*d_slot-.5*x
+            pos = (slot_x,holder_slot_y_offset)
+            slot_list.append((pos,holder_slot_size))
+
+        holder_hole_offset = self.params['holder_hole_offset']
+
+        # End holes for the holders
+        for i in (-1,1):
+            for j in (-1,1):
+                hole_x = i*j*.5*x_h - j*d_slot
+                for dy in holder_hole_offset:
+                    hole_y = dy
+                    hole_data = (hole_x,hole_y,dia)
+                    hole_list.append(hole_data)
+        # Special cases for a 5-device (or greater) rack
+        # need to add an extra support hole in the middle
+        if self.params['num_devices'] >= 5:
+            for dy in holder_hole_offset:
+                hole_data = (0,dy,dia)
+                hole_list.append(hole_data)
+
+        params = {
+            'size'       : (x_h,y_h,z_h),    
+            'radius'     : self.params['corner_radius'], 
+            'slots'      : slot_list,  
+            'hole_list'  : hole_list,
+            'tilt_angle' : self.params['tilt_angle']
+            }
+
+        holder_maker = Expresso_Holder(params)
+        self.holder = holder_maker.make()
+
     def __make_bracket(self):
         pass
+
     #########################################
-    # Enclosure Assembly
+    # Rack Assembly
     #########################################
     def get_assembly(self,**kwargs):
         """
@@ -33,10 +216,67 @@ class Expresso_Rack(object):
         generated assembly.
         """
         try:
-            show_sensor = kwargs['show_walls']
+            show_walls = kwargs['show_walls']
         except KeyError:
             show_walls = True
+        try:
+            show_floor = kwargs['show_floor']
+        except KeyError:
+            show_floor = True
+        try:
+            show_holders = kwargs['show_holders']
+        except KeyError:
+            show_holders = True
+        try:
+            explode = kwargs['explode']
+        except KeyError:
+            explode = (0,0,0)
+
         part_list = []
+        
+        ##
+        # Add walls to the assembly parts list.
+        ##
+        x,y,z = self.params['inner_dimensions']
+        thickness = self.params['wall_thickness']
+        wall_x_overhang = self.params['x_r_overhang']
+        exp_x,exp_y,exp_z = explode
+        x_w = x + 2*wall_x_overhang
+        y_w = z
+        z_w = thickness
+
+        y_shift = .5*y+.5*thickness + 2*exp_y
+        z_shift = .5*z+.5*thickness + exp_z
+        left_wall = Rotate(self.left_wall,a=90,v=(1,0,0))
+        left_wall = Translate(left_wall,v=(0,y_shift,z_shift))
+        right_wall = Rotate(self.right_wall,a=90,v=(1,0,0))
+        right_wall = Translate(right_wall,v=(0,-y_shift,z_shift))
+        if show_walls:
+            part_list.append(left_wall)
+            part_list.append(right_wall)
+
+        ##
+        # Add floor to the assembly parts list.
+        ##
+        z_shift = -exp_z
+        floor = Translate(self.floor,v=(0,0,z_shift))
+        if show_floor:
+            part_list.append(floor)
+
+        ##
+        # Add holders to the assembly parts list.
+        ##
+        y_h = self.params['holder_height']
+        y_shift =  .5*y - .5*thickness + exp_y
+        z_shift = .5*thickness + .5*y_h + self.params['holder_z_offset'] + exp_z
+        left_holder = Rotate(self.holder,a=90,v=(1,0,0))
+        left_holder = Translate(left_holder,v=(0,y_shift,z_shift))
+        right_holder = Rotate(self.holder,a=90,v=(1,0,0))
+        right_holder = Translate(right_holder,v=(0,-y_shift,z_shift))
+        if show_holders:
+            part_list.append(left_holder)
+            part_list.append(right_holder)
+
         return part_list
 
     def get_projection(self):
@@ -159,59 +399,6 @@ class Expresso_Rack(object):
         ##self.upper_shelf = upper_shelf
         #return 0
 
-    #def __make_walls(self):
-        #"""
-        #Creates the left and right side panels of the enclosure
-        #(plate w/ tabs).
-        #"""
-        #inner_x, inner_y, inner_z = self.params['wall_dimensions']
-        #tab_width = self.params['wall_tab_width']
-        #try:
-            #depth_adjust = self.params['tab_depth_adjust']
-        #except KeyError:
-            #depth_adjust = 0.0
-
-        ## Only apply tab depth adjustment to positive tabs
-        #tab_depth_pos = inner_z + depth_adjust 
-
-        ## Create tab data for yz faces of side panels
-        #yz_pos = []
-        #for loc in self.params['wall_tabs']:
-            #tab_data = (loc, tab_width, tab_depth_pos, '+')
-            #yz_pos.append(tab_data)
-
-        ## Pack panel data into parameters structure
-        #params = {
-                #'size'      : (inner_x, inner_y, inner_z),
-                #'radius'    : self.params['corner_radius'], 
-                #'xz+'       : [],
-                #'xz-'       : [],
-                #'yz+'       : yz_pos,
-                #'yz-'       : [],
-                #'hole_list' : self.params['wall_holes'],
-                #}
-
-        #plate_maker = Plate_W_Tabs(params)
-        #self.left_wall = plate_maker.make()
-        #self.right_wall = plate_maker.make()
-
-    #def __make_floor(self):
-        #"""
-        #Creates the floor of the enclosure (plate w/ slots).
-        #"""
-        #floor_x, floor_y, floor_z = self.params['floor_dimensions']
-        #slot_list = []
-        #for pos,size in zip(self.params['floor_slot_pos'],self.params['floor_slot_size']):
-            #slot_list.append((pos,size))
-        #params = {
-            #'size'      : (floor_x,floor_y,floor_z),    
-            #'radius'    : self.params['corner_radius'], 
-            #'slots'     : slot_list,  
-            #'hole_list' : self.params['floor_hole_list']
-            #}
-        #plate_maker = Plate_W_Slots(params)
-        #self.floor = plate_maker.make()
-
     #def __make_bracket(self):
         #filename = self.params['bracket_filename']
         #facet_list = stl_tools.read_stl(filename)
@@ -222,18 +409,60 @@ class Expresso_Rack(object):
         #self.bracket = Translate(Import_STL('stl/bracket.stl').cmd_str(),\
                                  #(-.5*shift_x,-shift_y+.5*.875*INCH2MM,\
                                   #-.5*shift_z))
-    
-    #def __make_decks(self):
-        #deck_x, deck_y, deck_z = self.params['deck_dimensions']
-        #slot_list = []
-        #for pos,size in zip(self.params['deck_slot_pos'],self.params['deck_slot_size']):
-            #slot_list.append((pos,size))
-        #params = {
-            #'size'       : (deck_x,deck_y,deck_z),    
-            #'radius'     : self.params['corner_radius'], 
-            #'slots'      : slot_list,  
-            #'hole_list'  : self.params['deck_hole_list'],
-            #'tilt_angle' : self.params['tilt_angle']
-            #}
-        #deck_maker = Capillary_Deck(params)
-        #self.deck = deck_maker.make()
+
+#########################################
+# Holder Class
+#########################################
+class Expresso_Holder(object):
+    """
+    A custom plate w/ slots that allows me to specify the tilt angle
+    of the slots.
+    """
+    def __init__(self, params):
+        self.params = params
+
+    def __add_slots(self):
+
+        hole_list = []
+
+        for pos, size in self.params['slots']:
+            x, y = size
+            z = 2*self.params['size'][2]
+            hole = Cube(size=(x, y, z))
+            pos_x, pos_y = pos
+            hole = Rotate(hole,v=(0,0,1),a=self.params['tilt_angle'])
+            hole = Translate(hole,v=[pos_x, pos_y, 0])
+            hole_list.append(hole)
+
+        self.plate = Difference([self.plate] + hole_list)
+
+    def __add_holes(self):
+
+        try:
+            holes = self.params['hole_list']
+        except KeyError:
+            holes = []
+        plate_x, plate_y, plate_z = self.params['size']
+        height = plate_z
+        cylinders = []
+        for x,y,r in holes:
+            c = Cylinder(h=4*height,r1=0.5*r, r2=0.5*r)
+            c = Translate(c,v=[x,y,0])
+            cylinders.append(c)
+        self.plate = Difference([self.plate] + cylinders)
+
+    def make(self):
+        try:
+            radius = self.params['radius']
+        except KeyError:
+            radius = None
+
+        if radius is None:
+            self.plate = Cube(size=self.params['size'])
+        else:
+            x,y,z = self.params['size']
+            self.plate = rounded_box(x, y, z, radius, round_z=False)
+
+        self.__add_slots()
+        self.__add_holes()
+        return self.plate
--- rack/make_expresso_rack.py
+++ rack/make_expresso_rack.py
@@ -7,14 +7,19 @@ import os.path
 import subprocess
 from py2scad import *
 from expresso_rack import *
-import math, sys
-#from arrayed_enclosure import Arrayed_Enclosure
-#from make_enclosure import params as arrayed_enclosure_params
+import sys
+
+#########################################
+# Rack configuration
+#########################################
+# Number of devices to include
+N = 5
+# Desired tilt angle on the rack (typical: -45)
+tilt_angle = -45 #degrees
+#########################################
 
 # Conversion constants
 INCH2MM = 25.4
-DEG2RAD = math.pi/180
-RAD2DEG = 180/math.pi
 
 # Inside dimensions enclosure
 x,y,z = 61.4, 8.0*INCH2MM, 0.75*INCH2MM 
@@ -27,8 +32,7 @@ y_e = y + 2.*wall_thickness_enc + 2.*bottom_y_overhang
 z_e = z
 
 #Inside dimensions rack
-N = 5
-wall_thickness_enc = 6.
+wall_thickness_rack = 6.
 x_r = N*2.5*INCH2MM
 y_r = y_e
 z_r = 6.*INCH2MM
@@ -37,30 +41,27 @@ hole_list = []
 
 params = {
         # Rack parameters
+        'num_devices'           : N,        
         'inner_dimensions'      : (x_r,y_r,z_r),
-        'wall_thickness'        : wall_thickness_enc,
+        'wall_thickness'        : wall_thickness_rack,
+        'wall_thickness_enc'    : wall_thickness_enc,
         'x_r_overhang'          : 1.*INCH2MM,
         'y_r_overhang'          : 1.*INCH2MM,
-        'num_devices'           : N,        
-  #'wall_tab_width'   : wall_tab_width,
-  #'floor_dimensions' : (x_floor,y_floor,z_floor),
-  #'deck_dimensions'  : (x_deck,y_deck,z_deck),
-  #'wall_tabs'        : (1/6,1/3,1/2,2/3,5/6),
-  #'wall_holes'       : wall_hole_list,
-  #'floor_slot_pos'   : floor_slot_pos,
-  #'floor_slot_size'  : floor_slot_size,
-  #'floor_hole_list'  : floor_hole_list,
-  #'deck_slot_pos'    : deck_slot_pos,
-  #'deck_slot_size'   : deck_slot_size,
-  #'deck_hole_list'   : deck_hole_list,
-  #'corner_radius'    : 1.5,
-  #'wall_overhang'    : wall_overhang,
-  #'bracket_filename' : 'stl/8020-4119.stl',
-  #'bracket_width'    : 1.0*INCH2MM,
-  #'enclosure_stl_path' : '/home/cisco/Work/capillary_sensor_enclosure/stl',
-  #'tilt_angle'         : -45,
-  #'projection_spacing' : .5*INCH2MM,
-  #'bs_shift_z'          : .25*INCH2MM
+        'corner_radius'         : 1.5,
+        'wall_tab_dist'         : 1.25*INCH2MM,
+        'wall_tab_width'        : 1.*INCH2MM,
+        'wall_hole_dia_thru'    : .2570*INCH2MM,
+        'wall_hole_dia_tap'     : .2010*INCH2MM,
+        'wall_hole_y_offset'    : .5*INCH2MM, # from the floors' surface
+        'floor_hole_y_offset'   : .5*INCH2MM, # from the floors' end, including overhang
+        'floor_hole_dia_thru'   : .2570*INCH2MM,
+        'holder_height'         : 4.*INCH2MM,
+        'holder_slot_size'      : (wall_thickness_enc,.6*x_e),
+        'floor_slot_tol'        : (.1*INCH2MM,.05*INCH2MM),
+        'tilt_angle'            : tilt_angle,
+        'holder_z_offset'       : 1.*INCH2MM, # from the floors' surface
+        'holder_hole_offset'  : (-1.75*INCH2MM,1.75*INCH2MM),
+        'stability_rod_dia'     : .5*INCH2MM,
         }
 
 # -----------------------------------------------------------------------------
@@ -73,7 +74,9 @@ if __name__ == '__main__':
 
     part_assembly = rack.get_assembly(
             show_walls=True,
-            explode=(0,0,4),
+            show_floor=True,
+            show_holders=True,
+            explode=(0,6,6),
             )
 
     # Write assembly scad file
@@ -82,3 +85,22 @@ if __name__ == '__main__':
     prog_assembly.add(part_assembly)
     prog_assembly.write('scad/rack_assembly.scad')
 
+# Legacy
+#'wall_tabs'        : (1/6,1/3,1/2,2/3,5/6),
+#'wall_tab_width'   : wall_tab_width,
+#'floor_dimensions' : (x_floor,y_floor,z_floor),
+#'deck_dimensions'  : (x_deck,y_deck,z_deck),
+#'wall_holes'       : wall_hole_list,
+#'floor_slot_pos'   : floor_slot_pos,
+#'floor_slot_size'  : floor_slot_size,
+#'floor_hole_list'  : floor_hole_list,
+#'deck_slot_pos'    : deck_slot_pos,
+#'deck_slot_size'   : deck_slot_size,
+#'deck_hole_list'   : deck_hole_list,
+#'corner_radius'    : 1.5,
+#'wall_overhang'    : wall_overhang,
+#'bracket_filename' : 'stl/8020-4119.stl',
+#'bracket_width'    : 1.0*INCH2MM,
+#'enclosure_stl_path' : '/home/cisco/Work/capillary_sensor_enclosure/stl',
+#'projection_spacing' : .5*INCH2MM,
+#'bs_shift_z'          : .25*INCH2MM
